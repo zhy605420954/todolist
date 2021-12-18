c209c869051244a50862a6a267dedf713d093e9b
zhy605420954 <605420954@qq.com>
2021-12-18 10:20:07 +0800

    springboot

---
+++ src/main/java/com/example/todolistdemo/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.todolistdemo.config;
+
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("vip1")
+                .antMatchers("/level2/**").hasRole("vip2")
+                .antMatchers("/level3/**").hasRole("vip3");
+
+        //无授权默认跳到登录页
+        http.formLogin();
+        //注销后回到首页
+        http.logout().logoutSuccessUrl("/");
+        http.rememberMe();
+
+    }
+//认证
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        //在内存中定义，也可在jdbc中拿
+        /*auth.inMemoryAuthentication()
+                .withUser("test").password("123456").roles("vip2","vip3")
+                .and()
+                .withUser("root").password("123456").roles("vip1","vip2","vip3")
+                .and()
+                .withUser("guest").password("123456").roles("vip2","vip1");
+*/
+        //这些数据正常应该中数据库中读
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("kuangshen").password(new BCryptPasswordEncoder().encode("123456")).roles("vip2","vip3")
+                .and()
+                .withUser("root").password(new BCryptPasswordEncoder().encode("123456")).roles("vip1","vip2","vip3")
+                .and()
+                .withUser("guest").password(new BCryptPasswordEncoder().encode("123456")).roles("vip1");
+
+    }
+}
